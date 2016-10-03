0dc34920ffbf6b387aa465a938336798019a9739
Thomas Fischer <tdf.tomfischer@gmail.com>
2016-10-02 22:31:41 -0700

    emacs.d: add matt levine (bloomberg columnist) to rss trackers

--- emacs.d/tom/tom-rss.el
+++ emacs.d/tom/tom-rss.el
@@ -3,5 +3,6 @@
 (global-set-key (kbd "C-x w") 'elfeed)
 (setq elfeed-feeds
       '("https://siajat.github.io/feed.xml"
-	"https://gragas.github.io/feed.xml"))
+	"https://gragas.github.io/feed.xml"
+	"https://www.bloomberg.com/view/rss/contributors/matt-levine.rss"))
 (provide 'tom-rss)
