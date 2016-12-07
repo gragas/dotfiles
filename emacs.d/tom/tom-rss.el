a8f9d4a898cb95f4d13d171734f3f5e6b6e120d3
Thomas Fischer <tdf.tomfischer@gmail.com>
2016-12-06 20:27:44 -0800

    emacs.d,wallpapers: add 'papes; add brennan.io to rss trackers

--- emacs.d/tom/tom-rss.el
+++ emacs.d/tom/tom-rss.el
@@ -4,5 +4,6 @@
 (setq elfeed-feeds
       '("https://siajat.github.io/feed.xml"
 	"https://gragas.github.io/feed.xml"
-	"https://www.bloomberg.com/view/rss/contributors/matt-levine.rss"))
+	"https://www.bloomberg.com/view/rss/contributors/matt-levine.rss"
+	"https://brennan.io/blog/rss.xml"))
 (provide 'tom-rss)
