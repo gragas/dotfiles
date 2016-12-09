4ba0300fd96470d527033f21417ef4242324581d
Thomas Fischer <thomas.fischer@coreos.com>
2016-12-09 14:34:07 -0800

    config/fish: add function to handle go command typos

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,6 +14,16 @@ function fish_prompt
 	set_color normal	
 end
 
+# go typos
+function go
+	if [ (count $argv) -eq 1 -a (contains $argv[1] "isntall" "istnall" "intsall"; echo $status) -eq 0 ]
+		echo "running 'go install' instead"
+		command go install
+	else
+		command go $argv
+	end
+end
+
 # Add /usr/local/bin to PATH
 set PATH $PATH "/usr/local/bin"
 
