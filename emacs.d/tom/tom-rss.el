f4b4f70785a5c5aaa2b4a073bde8288748387921
Thomas Fischer <tdf.tomfischer@gmail.com>
2016-12-08 17:54:14 -0800

    emacs.d: add Christian's wordpress site to rss trackers

--- emacs.d/tom/tom-rss.el
+++ emacs.d/tom/tom-rss.el
@@ -4,6 +4,7 @@
 (setq elfeed-feeds
       '("https://siajat.github.io/feed.xml"
 	"https://gragas.github.io/feed.xml"
+	"https://lilbbw.wordpress.com/feed/"
 	"https://www.bloomberg.com/view/rss/contributors/matt-levine.rss"
 	"https://brennan.io/blog/rss.xml"))
 (provide 'tom-rss)
