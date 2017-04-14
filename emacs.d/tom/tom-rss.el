9186be94e3635ef551fce481d3b3de989115ff36
Thomas Fischer <tdf.tomfischer@gmail.com>
2017-04-14 11:26:48 -0500

    emacs.d: update rss feeds list

--- emacs.d/tom/tom-rss.el
+++ emacs.d/tom/tom-rss.el
@@ -6,5 +6,15 @@
 	"https://gragas.github.io/feed.xml"
 	"https://lilbbw.wordpress.com/feed/"
 	"https://www.bloomberg.com/view/rss/contributors/matt-levine.rss"
-	"https://brennan.io/blog/rss.xml"))
+	"https://brennan.io/blog/rss.xml"
+	"http://randycoulman.com/atom.xml"
+	"https://medium.com/feed/@dan_abramov/"
+	"https://daveceddia.com/feed.xml"
+	"https://decembersoft.com/feed/"
+	"http://feeds.feedburner.com/TheJavascriptPlayground"
+	"http://feedpress.me/jlongster"
+	"https://www.robinwieruch.de/index.xml"
+	"http://feeds.feedburner.com/2ality"
+	"http://steve-yegge.blogspot.com/atom.xml"
+	"https://www.quantamagazine.org/feed/"))
 (provide 'tom-rss)
